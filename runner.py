8d2d722dad59b67525b2acea83184a2fefaf6e39
rchandra <rchandra@mit.edu>
2020-08-13 11:25:47 -0700

    width

--- runner.py
+++ runner.py
@@ -18,10 +18,10 @@ def my_form_post():
         '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">',
         '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">',
         '<style> body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif} .w3-bar,h1,button {font-family: "Montserrat", sans-serif}',
-        '.fa-anchor,.fa-coffee {font-size:200px}</style></head><body bgcolor = "FFBFAA"><center> <p></p><div style = "width: 50%; border: 1px solid #996955" id = "form" ><h1> Raag Alankar Generator',
+        '.fa-anchor,.fa-coffee {font-size:200px}</style></head><body bgcolor = "FFBFAA"><center> <p></p><div style = "width: 95%; border: 1px solid #996955" id = "form" ><h1> Raag Alankar Generator',
         '</h1><form action="/" method="POST"><h3> Raag Name </h3><input type="text" name="text1"> <br/><h3> Alankar Number </h3>',
         '<input type="text" name="text2"> <br/> <br/> <input type="submit" name="my-form" value="Get Alankars"><p></p></form> </div></center>',
-        '<center><div style = "width: 50%; border: 1px solid #996955" id = "form" ><p></p>'])
+        '<center><div style = "width:95%; border: 1px solid #996955" id = "form" ><p></p>'])
 
     text1 = request.form['text1']
     text2 = request.form['text2']
